502d9605ed0a7eb7097abd6a1e241f6b48b24225
Alex Valcourt Caron <alex.valcourt.caron@usherbrooke.ca>
2018-02-04 18:35:51 -0500

    Base code for eigen faces algo

---
+++ ProjectEigenFaces/Project1/ImageVector.h
@@ -0,0 +1,39 @@
+#pragma once
+#include <vector>
+#include "Eigen/Cholesky"
+#include "CImg.h"
+
+template<class T>
+class ImageVector
+{
+public:
+    typedef     Eigen::VectorXi                         VectorizedComponentType;
+    typedef     std::vector<VectorizedComponentType>    VectorizedImageType;
+    typedef     VectorizedImageType::iterator           VectorizedImageIteratorType;
+    typedef     VectorizedImageType::const_iterator     VectorizedImageConstIteratorType;
+
+    typedef     cimg_library::CImg<T>                   ImageType;
+
+    explicit ImageVector(ImageType image) { vectorize(image); };
+    ~ImageVector();
+
+    VectorizedImageConstIteratorType begin() { return imageComponents.begin(); }
+    VectorizedImageConstIteratorType end() { return imageComponents.end(); }
+
+    void clear() { imageComponents.clear(); }
+    void resize(const size_t size) { clear(); imageComponents.resize(size); }
+    void setComponent(const size_t, VectorizedComponentType);
+    
+    void setPixelSize(int size) { std::for_each(begin(), end(), [](VectorizedComponentType& component) { component.resize(size); }); }
+
+    size_t componentsCount() const { return imageComponents.size(); }
+    size_t pixelCount() const { return imageComponents.front().size(); }
+
+private:
+
+    void vectorize(ImageType);
+    void initialize(const int, const int);
+
+    VectorizedImageType     imageComponents;
+};
+
