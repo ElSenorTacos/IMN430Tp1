3f1aedc8211695d90ec4aed64a70168c81f3e998
Alex Valcourt Caron <alex.valcourt.caron@usherbrooke.ca>
2018-02-05 12:30:33 -0500

    Foreground extraction class

---
+++ ProjectEigenFaces/Project1/FGExtractor.cpp
@@ -0,0 +1,23 @@
+#include "FGExtractor.h"
+
+
+template <class T>
+FGExtractor<T>::FGExtractor()
+{
+}
+
+template <class T>
+FGExtractor<T>::~FGExtractor()
+{
+}
+
+template <class T>
+typename FGExtractor<T>::ImageType FGExtractor<T>::estimageForeground(ImageType image)
+{
+    ImageType
+}
+
+template <class T>
+std::vector<std::pair<int, int>> FGExtractor<T>::getForegroundPixelsPositions(ImageType image)
+{
+}
